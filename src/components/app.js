b928b95d6b8004db7d3cdbac420ac77a04c3a004
RdzCore <daniel_rod_con@hotmail.com>
2020-09-14 12:22:42 -0500

    built header and navbar components/styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import Header from "./navigation/header";
+import NavigationContainer from "./navigation/navigation-container";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className="container">
+        <Header />
+        <NavigationContainer />
         <h1>Lets build a eCommerce</h1>
       </div>
     );
