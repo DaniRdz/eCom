176f1b2a95add0e90c15e0f2b9a01844271f515d
RdzCore <daniel_rod_con@hotmail.com>
2020-09-14 13:48:11 -0500

    built a signin route component

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Header from "./navigation/header";
 import NavigationContainer from "./navigation/navigation-container";
+import SignIn from "./sign-in";
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
-        <Header />
-        <NavigationContainer />
-        <h1>Lets build a eCommerce</h1>
+        <BrowserRouter>
+          <Header />
+          <NavigationContainer />
+          <Switch>
+            <Route exact path="/" component={SignIn} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
