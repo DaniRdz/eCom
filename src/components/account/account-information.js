48d2f46dd9f79e7509f463eadf165b05f637e1db
RdzCore <daniel_rod_con@hotmail.com>
2020-09-24 13:14:30 -0500

    built account information from from signinform as a template and remove focus for de input

--- src/components/account/account-information.js
+++ src/components/account/account-information.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import PageTitle from "../page-title";
+import AccountInformationForm from "./account-information-form";
 
 export default class AccountInformation extends Component {
-    render() {
-        return (
-            <div>
-                Account Information
-            </div>
-        );
-    }
+  onSubmit = (fields) => {
+    console.log(fields);
+  };
+  render() {
+    return (
+      <div className="account-information">
+        <PageTitle
+          className="account-information"
+          title="Account Information"
+        />
+        <AccountInformationForm
+          onSubmit={this.onSubmit}
+          className="account-information-form"
+        />
+      </div>
+    );
+  }
 }
