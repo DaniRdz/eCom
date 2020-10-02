1a3f0bc759cb4584eea4bd86a020f7addebfdd25
RdzCore <daniel_rod_con@hotmail.com>
2020-10-02 11:47:43 -0500

    structure shop-cart component and styled

--- src/components/shop/shop-cart.js
+++ src/components/shop/shop-cart.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
 
+function CartButton(props) {
+  const { className, icon } = props;
+  return (
+    <div className={`${className} cart-button`}>
+      <i className={icon} />
+    </div>
+  );
+}
+
+function CartContent(props) {
+  const { className } = props;
+  return <div className={`${className} cart-content`}>...content</div>;
+}
+
 class ShopCart extends Component {
   render() {
     const { className } = this.props;
-    return <div className={`${className} cart`}>Shop Kart!!</div>;
+    return (
+      <div className={`${className} cart`}>
+        <CartButton className="shop-cart-toggle" icon="fas fa-times" />
+        <CartContent className="shop-cart-content" />
+      </div>
+    );
   }
 }
 
