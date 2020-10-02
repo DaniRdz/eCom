32dc073b8690c6f088536cec4fa3f1e40b54c7d2
RdzCore <daniel_rod_con@hotmail.com>
2020-10-02 12:54:56 -0500

    built shop-cart-content-footer component and styled the checkout button

--- src/components/shop/shop-cart.js
+++ src/components/shop/shop-cart.js
@@ -12,17 +12,28 @@ function CartButton(props) {
 function CartContent(props) {
   const { className, products } = props;
   let count = products.length;
-  return <div className={`${className} cart-content`}>
-      <div className='cart-content-title'>
-            Cart ({count})
-      </div>
-      <div className='cart-content-products'>
-           
-      </div>
-      <div className='cart-content-footer'>
+  let renderProducts = products.map((product) => {
+    return <h1 key={product}>{product}</h1>;
+  });
+  return (
+    <div className={`${className} cart-content`}>
+      <div className="cart-content-title">Cart ({count})</div>
+      <div className="cart-content-products"> {renderProducts}</div>
+      <CartFooter className="cart-content-footer" products={products} />
+    </div>
+  );
+}
 
-      </div>
-  </div>;
+function CartFooter(props) {
+  const { className, products } = props;
+  const price = 7.99;
+  return (
+    <div className={`${className} cart-footer`}>
+      <a className="cart-footer-checkout">Checkout</a>
+      <div className="cart-footer-subtotal">Subtotal</div>
+      <div className="cart-footer-price">${price}</div>
+    </div>
+  );
 }
 
 class ShopCart extends Component {
@@ -31,7 +42,10 @@ class ShopCart extends Component {
     return (
       <div className={`${className} cart`}>
         <CartButton className="shop-cart-toggle" icon="fas fa-times" />
-        <CartContent className="shop-cart-content" products={[123, 12, 21, 3]} />
+        <CartContent
+          className="shop-cart-content"
+          products={[123, 12, 21, 3]}
+        />
       </div>
     );
   }
