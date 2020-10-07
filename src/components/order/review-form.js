61d11c4b2d6c4bb5ce40a3ca0bad1e798bfb9b55
RdzCore <daniel_rod_con@hotmail.com>
2020-10-07 12:03:19 -0500

    review form styled grid

--- src/components/order/review-form.js
+++ src/components/order/review-form.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 
-import { FormInput, FormButton } from "../form-fields";
+import { FormButton } from "../form-fields";
 
 import history from "../../history";
 
@@ -14,7 +14,7 @@ class ReviewForm extends Component {
         <div className="review-form-line"></div>
         <Field
           className="review-form-proceed"
-          onClick={() => history.push("/account")}
+          onClick={() => history.push("/")}
           type="submit"
           title="Proceed To Checkout"
           name="proced"
@@ -22,7 +22,7 @@ class ReviewForm extends Component {
         />
         <Field
           className="review-form-back"
-          onClick={() => history.push("/")}
+          onClick={() => history.push("/shop")}
           type="button"
           title="Back"
           name="back"
