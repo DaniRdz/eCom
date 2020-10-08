78a26429a0ecd32bd9dde39273f5e9f01c64c686
RdzCore <daniel_rod_con@hotmail.com>
2020-10-08 11:54:55 -0500

    order review products title bar

--- src/components/order/review-form.js
+++ src/components/order/review-form.js
@@ -12,6 +12,11 @@ class ReviewForm extends Component {
 
     return (
       <form onSubmit={handleSubmit} className={`${className}`}>
+        <div className='review-form-legend'>
+          <div className='review-form-legend-name'>Name</div>
+          <div className='review-form-legend-quantity'>Quantity</div>
+          <div className='review-form-legend-price'>Price</div>
+        </div>
         <ReviewProducts className="review-form-products" />
         <div className="review-form-line"></div>
         <Field
