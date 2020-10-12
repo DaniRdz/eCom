8ac0570005b4857df1b4ef0422c6be8f22ff947c
RdzCore <daniel_rod_con@hotmail.com>
2020-10-12 11:39:12 -0500

    finished shipping form component and sub components

--- src/components/information/oder-summary.js
+++ src/components/information/oder-summary.js
@@ -1,31 +1,45 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import { UnderlinedTitle, InfoTitle } from "./info-details";
 
 class OrderSummary extends Component {
   render() {
     const { className } = this.props;
+    let subtotal = 0;
+    let tax = 0.16;
+    let countTickers = 0;
+    this.props.cartProducts.map((cartProduct) => {
+      subtotal += cartProduct.quantity * cartProduct.product.price;
+      countTickers += cartProduct.quantity;
+    });
     return (
       <div className={`${className} order-sumary`}>
         <UnderlinedTitle className="order-sumary-title" title="Order Summary" />
         <InfoTitle
           className="order-summary-subtotal"
-          title="4 stickers"
-          value="$7.96"
+          title={`${countTickers} sticker`}
+          value={`$${subtotal}`}
         />
         <InfoTitle
           className="order-summary-tax-shipping"
           title="Taxes & Shipping"
-          value="$0.16"
+          value={`$${tax}`}
         />
         <InfoTitle
-          className="order-summary-total"
+          className="order-summary-total info-title-green"
           title="Total"
-          value="$8.12"
+          value={`$${tax+subtotal}`}
         />
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  const { cartProducts } = state.user;
+  return { cartProducts };
+}
+OrderSummary = connect(mapStateToProps)(OrderSummary);
 
 export default OrderSummary;
